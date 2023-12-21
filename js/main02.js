126ad2b2c8e56aeb369654d41bb6678649c36aef
kimse17 <seongeun1117@naver.com>
2023-12-21 18:05:32 +0900

    Create main02.js

---
+++ js/main02.js
@@ -0,0 +1,32 @@
+const home = document.querySelector('.home_container');
+const homeHeight = home.offsetHeight;
+const top_button =document.querySelector(".arrow_up")
+
+document.addEventListener('scroll', () => {
+
+    if(window.scrollY > homeHeight / 2) {
+
+        top_button.style.opacity = 1;
+
+    } else {
+
+        top_button.style.opacity = 0;
+
+    }
+
+})
+
+const testimonal = document.querySelector('.testimonial_all')
+document.addEventListener('scroll', () => {
+
+    if(window.scrollY > 600) {
+
+        testimonal.style.color = red;
+
+    } else {
+
+        testimonal.style.color = black;
+
+    }
+
+})
