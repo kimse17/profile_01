c3c5a7182f560342dfb195004ace114ab77c8db1
kimse17 <seongeun1117@naver.com>
2023-12-26 18:03:16 +0900

    Create main03.js

---
+++ js/main03.js
@@ -0,0 +1,19 @@
+
+const categories = document.querySelector('.category');
+const projects = document.querySelectorAll('.project');
+categories.addEventListener('click', (event) => {
+    console.log(event);
+    const filter = event.target.dataset.category;
+    console.log(filter);
+    projects.forEach((project) => {
+        console.log(project.dataset.type);
+        if (filter === 'all' || filter === project.dataset.type) {
+            project.style.display = 'block';
+        } else {
+            project.style.display = 'none';
+        }
+        if (filter == null) {
+            return;
+        } 
+    })
+})
