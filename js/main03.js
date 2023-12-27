0c73933a84205a724e1e5eabb76c666c69830c18
kimse17 <seongeun1117@naver.com>
2023-12-27 13:26:33 +0900

    Update main03.js

--- js/main03.js
+++ js/main03.js
@@ -14,6 +14,18 @@ categories.addEventListener('click', (event) => {
         }
         if (filter == null) {
             return;
-        } 
+        }
     })
+})
+categories.addEventListenerr('click', (event) => {
+    console.log(event);
+    const filter = event.target.dataset.category;
+    console.log(filter);
+    projects.forEach(project.dataset.type);
+    if(filter === 'all' || filter === project.dataset.type){
+        project.style.display ='block';
+    } else {project.style.display = 'none';}
+    if(filter == null){
+        return;
+    }
 })
